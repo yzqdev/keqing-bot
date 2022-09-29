17fdaa358443950ea865dfd84e2abb16b5f082a2
yzqdev <yzqdev@outlook.com>
2022-09-29 20:02:58 +0800

    refuse invite

--- src/event/requestEvent.ts
+++ src/event/requestEvent.ts
@@ -4,12 +4,12 @@ import { AbstractEvent } from "./abstractEvent";
 export class RequestEvent extends AbstractEvent {
   public load(bot: Client): void {
     // 同意好友申请
-    bot.on("request.friend", (e) => e.approve());
+    bot.on("request.friend", (e) => e.approve(false));
 
     // 同意群邀请
-    bot.on("request.group.invite", (e) => e.approve());
+    bot.on("request.group.invite", (e) => e.approve(false));
 
     // 同意加群申请，拒绝`e.approve(false)`
-    bot.on("request.group.add", (e) => e.approve());
+    bot.on("request.group.add", (e) => e.approve(false));
   }
 }
